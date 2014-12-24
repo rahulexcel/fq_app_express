63bd2961b36432831297254adec707f6c094f2fd
manish <manish@excellencetechnologies.in>
2014-12-24 10:47:15 +0530

    1

--- app.js
+++ app.js
@@ -24,9 +24,17 @@ app.set('view engine', 'jade');
 //app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(function (req, res, next) {
+    console.log(req.body);
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Cache-Control, Pragma, Origin, Authorization, Content-Type, X-Requested-With");
+    res.header("Access-Control-Allow-Methods", "GET, PUT, POST");
+    next();
+});
 
 app.use(module_conn_pricegenie(mongoose));
 app.use(module_conn_db_scrap_db3(mongoose));
@@ -34,12 +42,12 @@ app.use(module_config());
 app.use(module_category());
 app.use(module_filters());
 // work accroding to version basis 
-app.use('/v1/catalog',v1_routes_catalog);
+app.use('/v1/catalog', v1_routes_catalog);
 //app.use('/products',routes_catalog); //arun :: 2nd step
 
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
     var err = new Error('Not Found');
     err.status = 404;
     next(err);
@@ -50,7 +58,7 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
+    app.use(function (err, req, res, next) {
         res.status(err.status || 500);
         res.render('error', {
             message: err.message,
@@ -61,7 +69,7 @@ if (app.get('env') === 'development') {
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
     res.status(err.status || 500);
     res.render('error', {
         message: err.message,
--- routes/v1/catalog.js
+++ routes/v1/catalog.js
@@ -1,13 +1,13 @@
 var express = require('express');
 var router = express.Router();
 //console.log('res resr resr');
-router.get('/list', function(req, res) {
+router.post('/list', function (req, res) {
     res.json(req.catalog);
 });
-router.get('/products',function(req,res){
+router.post('/products', function (req, res) {
     var where = {};
-    var finalData  = {
-        'filters':req.filters_to_show,
+    var finalData = {
+        'filters': req.filters_to_show,
     };
     // --params are available in req.body -- in modules/config.js files
     var products_per_page = req.config.products_per_page;
@@ -18,33 +18,33 @@ router.get('/products',function(req,res){
     var father_text = params.father_text;
     var page = params.page;
     var skip_count = (page - 1) * products_per_page;
-    console.log('page : '+ page);
-    console.log('per page : '+ products_per_page);
+    console.log('page : ' + page);
+    console.log('per page : ' + products_per_page);
     console.log('skip :' + skip_count);
     var website_scrap_data = req.conn_website_scrap_data;
-    
+
     var m_cat_id = cat_id;
     var m_sub_cat_id = sub_cat_id;
-    if( m_cat_id == '' || m_cat_id == false ){
+    if (m_cat_id == '' || m_cat_id == false) {
         //m_cat_id = 0;
         where['cat_id'] = m_cat_id;
     }
-    if( m_sub_cat_id == '' || m_sub_cat_id == false ){
+    if (m_sub_cat_id == '' || m_sub_cat_id == false) {
         //m_sub_cat_id=0;
         where['sub_cat_id'] = m_sub_cat_id;
     }
-    
+
     //if( cat_id == false ){
-        //cat_id = 0;
-   // }
-    
+    //cat_id = 0;
+    // }
+
     //if( cat_id != false ){
-        //where['cat_id'] = m_cat_id;
+    //where['cat_id'] = m_cat_id;
     //}
-    
+
     //var where = {
-        //'cat_id':m_cat_id,
-        //'sub_cat_id':m_sub_cat_id
+    //'cat_id':m_cat_id,
+    //'sub_cat_id':m_sub_cat_id
     //};
     //-start-process set filters----------
     function stringToArray(str, expby) {
@@ -60,28 +60,28 @@ router.get('/products',function(req,res){
         return ret;
     }
     var applied_filters = params.filters;
-    if( applied_filters.length > 0){
+    if (applied_filters.length > 0) {
         console.log('applied filters');
         console.log(applied_filters);
         console.log('--------');
-        Object.keys(applied_filters).forEach(function(key){
+        Object.keys(applied_filters).forEach(function (key) {
             fltr = applied_filters[key];
-            fltr_str_arr = stringToArray( fltr, '__');
+            fltr_str_arr = stringToArray(fltr, '__');
             check = fltr_str_arr[0];
-            if( check == 'filter' ){
+            if (check == 'filter') {
                 fltr_type = fltr_str_arr[1];
                 fltr_key = fltr_str_arr[2];
                 fltr_val = fltr_str_arr[3];
-                if( fltr_type == 'text'){
+                if (fltr_type == 'text') {
                     fltr_val = fltr_val.replace(/_/g, ' ');
                     where[fltr_key] = new RegExp(fltr_val, "i");
-                }else if(  fltr_type == 'range'){
-                    range_arr = stringToArray( fltr_val ,'_');
+                } else if (fltr_type == 'range') {
+                    range_arr = stringToArray(fltr_val, '_');
                     fltr_val_low = range_arr[0];
                     fltr_val_high = range_arr[1];
                     where[fltr_key] = {
-                        '$gte':fltr_val_low*1,
-                        '$lte':fltr_val_high*1
+                        '$gte': fltr_val_low * 1,
+                        '$lte': fltr_val_high * 1
                     };
                 }
             }
@@ -91,36 +91,36 @@ router.get('/products',function(req,res){
     console.log('where');
     console.log(where);
     console.log('--------');
-    
-    
-     website_scrap_data.where( where ).skip( skip_count ).limit( products_per_page ).find( query_results );
-    function query_results( err, data ){
+
+
+    website_scrap_data.where(where).skip(skip_count).limit(products_per_page).find(query_results);
+    function query_results(err, data) {
         finalData.products = data;
         res.json(finalData);
     }
-    
+
     /*
-    
-    website_scrap_data.findOne({
-        'cat_id':cat_id,
-    },function(err,result){
-        console.log('query run hui') ;
-        console.log(result.length);
-        res.json(result.length);
-    }).limit(10);
-    
-    */
+     
+     website_scrap_data.findOne({
+     'cat_id':cat_id,
+     },function(err,result){
+     console.log('query run hui') ;
+     console.log(result.length);
+     res.json(result.length);
+     }).limit(10);
+     
+     */
     //res.json(req.body);
     //res.json('is products page');
 });
-router.get('/quickview/:mid',function(req,res){
+router.get('/quickview/:mid', function (req, res) {
     var mid = req.param('mid'); // product mongo id
     res.json(mid);
 });
 //router.get('/list', function(req, res,next) {
-    // parameters will be found in body
-    //var body =req.body;
-    //body.cat_id''
-    //res.json('cat subc asdasd age');
+// parameters will be found in body
+//var body =req.body;
+//body.cat_id''
+//res.json('cat subc asdasd age');
 //});
 module.exports = router;
