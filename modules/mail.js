c15b94e01ca47508c2a3a5fcef8eb96b4e6264fb
upexcel <rahul.excel2011@gmail.com>
2017-05-30 11:23:22 +0530

    Send grid credentials removed by arun

--- modules/mail.js
+++ modules/mail.js
@@ -32,38 +32,7 @@ module.exports = function () {
             }, function(e) {
                 console.log('A mandrill error occurred: ' + e.name + ' - ' + e.message);
             });
-            /*
-            var mailer = nodemailer.createTransport(smtpTransport({
-//                host: 'smtp.mandrillapp.com',
-//                port: 587,
-//                auth: {
-//                    user: 'manish@excellencetechnologies.in',
-//                    pass: 'Ca4nS3QStEcpvZdk9iMh0Q'
-//                }
-
-                host: 'smtp.sendgrid.net',
-                port: 587,
-                auth: {
-                    user: 'manish_iitg',
-                    pass: 'java@123'
-                }
-
-            }));
-            mailer.use('compile', hbs(options));
-            mailer.sendMail({
-                from: 'noreply@fashioniq.in',
-                to: to,
-                subject: subject,
-                template: template,
-                context: context
-            }, function (error, response) {
-                if( error ){
-                    console.log(error);
-                }
-                console.log('mail sent to ' + to);
-                mailer.close();
-            });
-            */
+           
         }
     };
     return function (req, res, next) {
